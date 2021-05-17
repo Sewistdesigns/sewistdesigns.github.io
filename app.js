12010317b9bc64cae689f7c9e8f5399a4145077a
Sewistdesigns <e.m.flipsen@student.tue.nl>
2021-05-17 23:15:29 +0200

    app

--- app.js
+++ app.js
@@ -31,9 +31,9 @@ function draw(e) {
   ctx.lineCap = "round"; // rounded end cap
   ctx.strokeStyle = color; // hex color of line
 
-  ctx.moveTo(pos.x, pos.y - 60); // from position
+  ctx.moveTo(pos.x, pos.y - 80); // from position
   setPosition(e);
-  ctx.lineTo(pos.x, pos.y-60); // to position
+  ctx.lineTo(pos.x, pos.y-80); // to position
 
   ctx.stroke(); // draw it!
 }
